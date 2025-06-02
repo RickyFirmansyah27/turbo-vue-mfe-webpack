92f1f2b8d57bfd01c8f99969858ed8b728227118
RickyFirmansyah27 <ricky_firmansyah@teknokrat.ac.id>
2025-06-02 18:24:51 +0700

    add dropdown component

---
+++ apps/common/src/components/Dropdown.vue
@@ -0,0 +1,46 @@
+<template>
+  <div :class="wrapperClass">
+    <label v-if="label" class="block text-gray-700 font-medium mb-2">
+      {{ label }}
+    </label>
+    <select
+      :value="modelValue"
+      @change="$emit('update:modelValue', $event.target.value)"
+      :class="[
+        'w-full border rounded-md px-4 py-2 focus:outline-none focus:ring focus:ring-blue-200',
+        selectClass
+      ]"
+    >
+      <option
+        v-for="option in options"
+        :key="option.id"
+        :value="option.key"
+        :disabled="option.key === null"
+      >
+        {{ option.descriptions }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dropdown',
+  props: {
+    modelValue: [String, Number, null],
+    options: {
+      type: Array,
+      required: true,
+    },
+    label: String,
+    wrapperClass: {
+      type: String,
+      default: '',
+    },
+    selectClass: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
--- apps/shell/src/views/RemoteAppWrapper.vue
+++ apps/shell/src/views/RemoteAppWrapper.vue
@@ -7,18 +7,35 @@
       </p>
       <RemoteFeature />
     </div>
+
+    <e-select
+      v-model="selectedCategory"
+      :options="categoryOptions"
+      label="Select Category"
+      wrapperClass="mb-4"
+      selectClass="border-gray-300"
+    />
   </div>
 </template>
 
 <script>
+import { Dropdown } from 'commons/Components'
+
 export default {
   name: 'RemoteAppWrapper',
-   components: {
-   RemoteFeature: () => import('remote/Feature'),
+  components: {
+    RemoteFeature: () => import('remote/Feature'),
+    'e-select': Dropdown,
   },
   data() {
     return {
-      remoteAppComponent: null
+      selectedCategory: '',
+      categoryOptions: [
+        { id: 0, key: null, descriptions: 'Silahkan Pilih Category' },
+        { id: 1, key: 'tech', descriptions: 'Technology' },
+        { id: 2, key: 'health', descriptions: 'Health' },
+        { id: 3, key: 'edu', descriptions: 'Education' },
+      ],
     }
   },
 }
